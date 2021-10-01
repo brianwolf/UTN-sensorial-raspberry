335d33276ea4f7b3a9a3da26d798e6e21d926963
brianwolf <brian.d.lobo@gmail.com>
2021-09-30 22:50:18 -0300

    Se deja el hardcodeador con mismas fechas

--- logic/service.py
+++ logic/service.py
@@ -193,7 +193,7 @@ def random_date(date_init: datetime = None, date_final: datetime = None, time_de
 
 def _random_date(time_delta: int):
     cte = 10
-    time_rnd = random.randint(1, cte) * time_delta * 1000
+    time_rnd = random.randint(1, cte) * time_delta * 1000 + random.randint(0, 500)
 
     time_add = timedelta(milliseconds=time_rnd)
     return datetime.now() + time_add * (-1)**(random.randint(1, 2))
